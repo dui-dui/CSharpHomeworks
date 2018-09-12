f293dd06aeff45e7013c2f38881c1f173c90c3d8
1315200217 <1315200217@qq.com>
2018-09-12 21:29:15 +0800

    homework1

---
+++ homework1/program1/program1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            int a = 0, b = 0;
+            Console.Write("Please input an number:");
+            s = Console.ReadLine();
+            a = Int32.Parse(s);
+            Console.Write("Please input an number:");
+            s = Console.ReadLine();
+            b = Int32.Parse(s);
+            Console.WriteLine("a+b=" + (a + b));
+        }
+    }
+}
---
+++ homework1/program2/program2/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace program2
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string a = textBox1.Text;
+            string b = textBox2.Text;
+            double c = double.Parse(a);
+            double d = double.Parse(b);
+            double sq = c * d;
+            textBox3.Text = "乘积是" + sq;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
